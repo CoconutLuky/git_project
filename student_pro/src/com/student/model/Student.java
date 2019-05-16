e3012bd40effe5c1b01fefc7c1b31e15296a639f
CoconutLuky <49502310+CoconutLuky@users.noreply.github.com>
2019-05-16 14:34:36 +0800

    Update Student.java

--- student_pro/src/com/student/model/Student.java
+++ student_pro/src/com/student/model/Student.java
@@ -5,7 +5,7 @@ public class Student {
 	private String username;//用户名
 	private String password;//密码
 	private int sex;//性别
-	private String love;//爱好
+	private String love;
 	public int getId() {
 		return id;
 	}
